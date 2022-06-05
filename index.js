ed8297b9d8b503738df1e347c6b4d9dd93c4d8f6
fajar-dwi <fajardwi60xxx@gmail.com>
2022-06-05 15:57:44 +0700

    assign challenge

--- index.js
+++ index.js
@@ -6,15 +6,30 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (names, sort) => {
+  const hasil = sort(names);
+  return printHasil(hasil);
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (names) => {
+  return names.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (names) => {
+  return names.reverse();
+};
+
+const printHasil = (sort) => {
+  let display = [];
+  for(let i = 0 ; i < sort.length ; i++){
+    display.push(`${(i+1)}. ${sort[i]}`);
+  }
+  return display;
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
